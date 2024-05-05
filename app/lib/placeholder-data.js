4b2cff1ccd35ac0b3ceb7a48c76c909af9eaeec6
Christine <tsy17321289030@163.com>
2024-05-05 10:24:19 +0800

    feat: done

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -12,20 +12,20 @@ const users = [
 const customers = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
+    name: 'Christine',
     email: 'delba@oliveira.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
+    name: 'Picker',
     email: 'lee@robinson.com',
     image_url: '/customers/lee-robinson.png',
   },
   {
     id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
-    name: 'Hector Simpson',
-    email: 'hector@simpson.com',
+    name: 'Berry',
+    email: 'hector@berry.com',
     image_url: '/customers/hector-simpson.png',
   },
   {
